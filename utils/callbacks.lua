e627927ec798f75a8fdfb42416e4c789c9729b47
NahRyan <112652645+NahRyan@users.noreply.github.com>
2025-02-17 18:19:32 -0500

    Added client exports

---
+++ utils/callbacks.lua
@@ -0,0 +1,48 @@
+function GetAllOnDutyPlayers()
+    local data = lib.callback.await('SiriusDuty:GetAllOnDutyPlayers', false)
+    return data
+end
+
+exports("GetAllOnDutyPlayers", GetAllOnDutyPlayers)
+
+function GetPlayerDepartment()
+    local data = lib.callback.await('SiriusDuty:GetPlayerDepartment', false)
+    return data
+end
+
+exports("GetPlayerDepartment", GetPlayerDepartment)
+
+function IsPlayerManagement()
+    local data = lib.callback.await('SiriusDuty:IsPlayerManagement', false)
+    return data
+end
+
+exports("IsPlayerManagement", IsPlayerManagement)
+
+function IsPlayerStaff()
+    local data = lib.callback.await('SiriusDuty:IsPlayerStaff', false)
+    return data
+end
+
+exports("IsPlayerManagement", IsPlayerManagement)
+
+function GetPlayerCallsign()
+    local data = lib.callback.await('SiriusDuty:GetPlayerCallsign', false)
+    return data
+end
+
+exports("GetPlayerCallsign", GetPlayerCallsign)
+
+function GetDepartmentLogo()
+    local data = lib.callback.await('SiriusDuty:GetDepartmentLogo', false)
+    return data
+end
+
+exports("GetDepartmentLogo", GetDepartmentLogo)
+
+function GetDeptLongName(name)
+    local data = lib.callback.await('SiriusDuty:GetDeptLongName', false, name)
+    return data
+end
+
+exports("GetDeptLongName", GetDeptLongName)
